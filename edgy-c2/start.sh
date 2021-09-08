9c7b482aeaeadaf26b104b1d1cd62d484e0c1708
Diego Vicedo <diegovicedo@MacBook-Pro-de-Diego.local>
2021-09-08 00:11:49 -0300

    typo fixed

--- edgy-c2/start.sh
+++ edgy-c2/start.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gopish-docker-compose.yml up -d
+docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gophish-docker-compose.yml up -d
--- edgy-c2/stop.sh
+++ edgy-c2/stop.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gopish-docker-compose.yml stop
+docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gophish-docker-compose.yml stop
