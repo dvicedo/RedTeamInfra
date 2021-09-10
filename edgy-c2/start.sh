27a176e4a78991accdf0d381d1fc171c5d28d0fc
Diego Vicedo <diegovicedo@MacBook-Pro-de-Diego.local>
2021-09-10 15:49:19 -0300

    fixing network docker problems and adding tail to maintain containers running

--- edgy-c2/start.sh
+++ edgy-c2/start.sh
@@ -1,1 +1,3 @@
-docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gophish-docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d 
+docker-compose -f covenant-docker-compose.yml up -d
+docker-compose -f gophish-docker-compose.yml up -d
