09de09f3f26c7f62836fd6bc98c4ce08a2314752
Diego Vicedo <diegovicedo@MacBook-Pro-de-Diego.local>
2021-09-13 11:47:58 -0300

    merlin added

--- edgy-c2/start.sh
+++ edgy-c2/start.sh
@@ -1,3 +1,4 @@
 docker-compose -f docker-compose.yml up -d 
 docker-compose -f covenant-docker-compose.yml up -d
 docker-compose -f gophish-docker-compose.yml up -d
+docker-compose -f merlin-docker-compose.yml up -d
