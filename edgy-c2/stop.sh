7eb38c7793960150f00faaf8fd3ebf477c170bbe
Diego Vicedo <diegovicedo@MacBook-Pro-de-Diego.local>
2021-09-13 17:00:05 -0300

    added new fix

--- edgy-c2/stop.sh
+++ edgy-c2/stop.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gophish-docker-compose.yml -f merlin-docker-compose.yml stop
+docker-compose -f docker-compose.yml -f covenant-docker-compose.yml -f gophish-docker-compose.yml -f merlin-docker-compose.yml down --remove-orphans
